edf31e5e91c89c7d23441ae0910983aacb7b84ac
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 11:39:08 +0200

    refactor

--- src/main/scala/Raindrops.scala
+++ src/main/scala/Raindrops.scala
@@ -1,4 +1,6 @@
 object Raindrops:
-   def convert(n: Int): String =
-      val vs = Map(3 -> 'i', 5 -> 'a', 7 -> 'o').filter(n % _._1 == 0).map(t => s"Pl${t._2}ng")
-      if vs.isEmpty then n.toString else vs.mkString
+   def convert(n: Int): String = Map(3 -> 'i', 5 -> 'a', 7 -> 'o')
+      .filter(n % _._1 == 0)
+      .map(t => s"Pl${t._2}ng")
+      .reduceOption(_ + _)
+      .getOrElse(n.toString)
