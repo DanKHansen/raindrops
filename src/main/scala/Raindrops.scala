637b596b470101e9f19c9f4e1a2b75d972ac13ca
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 10:01:33 +0200

    solution

--- src/main/scala/Raindrops.scala
+++ src/main/scala/Raindrops.scala
@@ -1,2 +1,12 @@
 object Raindrops:
-   def convert(n: Int): String = ???
+   def convert(n: Int): String =
+      val factors: (Int, Int, Int) = (n % 3, n % 5, n % 7)
+      factors match
+         case (0, i, j) if i != 0 & j != 0 => "Pling"
+         case (i, 0, j) if i != 0 & j != 0 => "Plang"
+         case (i, j, 0) if i != 0 & j != 0 => "Plong"
+         case (0, 0, i) if i != 0          => "PlingPlang"
+         case (0, i, 0) if i != 0          => "PlingPlong"
+         case (i, 0, 0) if i != 0          => "PlangPlong"
+         case (0, 0, 0)                    => "PlingPlangPlong"
+         case _                            => n.toString
