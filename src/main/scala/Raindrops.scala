65f09c9dd60da2caef2bf85a16f94d026ab5826b
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 10:43:36 +0200

    refactor

--- src/main/scala/Raindrops.scala
+++ src/main/scala/Raindrops.scala
@@ -1,5 +1,5 @@
 object Raindrops:
    def convert(n: Int): String =
-      Seq(3 -> "Pling", 5 -> "Plang", 7 -> "Plong").collect { case (f, s) if n % f == 0 => s }.mkString match
-         case ""     => n.toString
-         case sounds => sounds
+      Seq(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
+        .collect { case (f, s) if n % f == 0 => s }
+        .reduceOption(_ + _).getOrElse(n.toString)
