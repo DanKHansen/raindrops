e6d715b9f7165e00de071f7a2b8db3f5c6d88681
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 11:09:21 +0200

    refactor

--- src/main/scala/Raindrops.scala
+++ src/main/scala/Raindrops.scala
@@ -1,4 +1,4 @@
 object Raindrops:
    def convert(n: Int): String =
-      val vs = Map(3 -> "Pling", 5 -> "Plang", 7 -> "Plong").filter(n % _._1 == 0).values
-      if vs.isEmpty then n.toString else vs.mkString
+      val vs = Map(3 -> "i", 5 -> "a", 7 -> "o").filter(n % _._1 == 0).values
+      if vs.isEmpty then n.toString else vs.map(s => s"Pl${s}ng").mkString
