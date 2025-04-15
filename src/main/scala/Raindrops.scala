e40b4976b5f13f15eafb6773db255678ec2d91c2
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 10:24:48 +0200

    refactor

--- src/main/scala/Raindrops.scala
+++ src/main/scala/Raindrops.scala
@@ -1,9 +1,7 @@
 object Raindrops:
    def convert(n: Int): String =
-      val factors = (n % 3, n % 5, n % 7)
-      val sounds = Seq(
-        if factors._1 == 0 then "Pling" else "",
-        if factors._2 == 0 then "Plang" else "",
-        if factors._3 == 0 then "Plong" else ""
-      ).mkString
-      if sounds.isEmpty then n.toString else sounds
+      Map(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
+         .filter(n % _._1 == 0)
+         .values
+         .reduceOption(_ + _)
+         .getOrElse(n.toString)
