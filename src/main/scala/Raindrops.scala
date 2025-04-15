8f8e33bd77bc2b743fac5bc69c0829798a4d6e45
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 11:02:16 +0200

    refactor

--- src/main/scala/Raindrops.scala
+++ src/main/scala/Raindrops.scala
@@ -1,5 +1,4 @@
 object Raindrops:
    def convert(n: Int): String =
-      Seq(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
-        .collect { case (f, s) if n % f == 0 => s }
-        .reduceOption(_ + _).getOrElse(n.toString)
+      val s = Map(3 -> "Pling", 5 -> "Plang", 7 -> "Plong").filter(n % _._1 == 0).values
+      if s.isEmpty then n.toString else s.mkString
